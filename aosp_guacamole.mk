1aee3f39a6b1044c27502274d78bce5b4ac71548
itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>
2020-12-21 19:40:45 -0500

    guacamole: Set flag for boot animation
    Signed-off-by: itsjoeoui <xiaowei.yu@dawsoncollege.qc.ca>

--- aosp_guacamole.mk
+++ aosp_guacamole.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamole/device.mk)
 
 # Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1440
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
